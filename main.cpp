def82404a0516a57410a3e24a0e6302d6c1f3bc5
Simon Chen <simonc8192@gmail.com>
2025-05-14 15:24:41 +0800

    init SFML and ImGui

--- main.cpp
+++ main.cpp
@@ -1,21 +1,49 @@
 #include <SFML/Graphics.hpp>
 
+#include "imgui.h"
+#include "imgui-SFML.h"
+
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    sf::RenderWindow window(sf::VideoMode({ 1280, 720 }), "SFML works!");
+    ImGui::SFML::Init(window);
 
+    bool drawCircle = true;
+    float circleRadius = 200.0f;
+    int circleSegments = 100;
+    sf::CircleShape shape(circleRadius, circleSegments);
+    shape.setFillColor(sf::Color::Green);
+    shape.setPosition({ 200, 200 });
+
+    sf::Clock deltaClock;
     while (window.isOpen())
     {
         while (const std::optional event = window.pollEvent())
         {
+            ImGui::SFML::ProcessEvent(window, *event);
+
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::Begin("Window Title");
+        ImGui::Text("Window text!");
+        ImGui::Checkbox("Circle", &drawCircle);
+        ImGui::SliderFloat("Radius", &circleRadius, 100.0f, 300.0f);
+        ImGui::SliderInt("Segments", &circleSegments, 3, 150);
+        ImGui::End();
+
+        shape.setRadius(circleRadius);
+        shape.setPointCount(circleSegments);
 
         window.clear();
-        window.draw(shape);
+        if (drawCircle)
+            window.draw(shape);
+        ImGui::SFML::Render(window);
         window.display();
     }
+
+    ImGui::SFML::Shutdown();
+    return 0;
 }
