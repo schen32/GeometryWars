bfd24f0d3d200d7728bac9e1f6fad909aa4aac83
Simon Chen <simonc8192@gmail.com>
2025-05-15 12:16:05 +0800

    Game.h skeleton for Game

---
+++ Game.cpp
@@ -0,0 +1,8 @@
+#include "Game.h"
+
+#include <iostream>
+
+class Game
+{
+
+};
---
+++ Game.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include "imgui-SFML.h"
+#include "imgui.h"
+#include "EntityManager.hpp"
+
+class Game
+{
+	sf::RenderWindow m_window;
+	EntityManager m_entities;
+	sf::Font m_font;
+	sf::Text m_text;
+	PlayerConfig m_playerConfig;
+	EnemyConfig m_enemyConfig;
+	BulletConfig m_bulletConfig;
+	sf::Clock m_deltaClock;
+	int m_score = 0;
+	int m_currentFrame = 0;
+	int m_lastEnemySpawnTime = 0;
+	bool m_paused = false;
+	bool m_running = true;
+
+	void init(const std::string& config);
+	void setPaused(bool paused);
+
+	void sMovement();
+	void sUserInput();
+	void sLifespan();
+	void sRender();
+	void sGUI();
+	void sEnemySpawner();
+	void sCollision();
+
+	void spawnPlayer();
+	void spawnEnemy();
+	void spawnSmallEnemies(std::shared_ptr<Entity> entity);
+	void spawnBullet(std::shared_ptr<Entity> entity, const Vec2f& mousePos);
+	void spawnSpecialWeapon(std::shared_ptr<Entity> entity);
+
+	std::shared_ptr<Entity> player();
+
+public:
+	Game(const std::string& config);
+
+	void run();
+
+};
